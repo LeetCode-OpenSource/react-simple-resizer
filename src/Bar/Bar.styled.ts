c5b499097948217abf329d5881e0bbd089264ed4
Runjuu <huang@runjuu.com>
2019-05-31 14:20:36 +0800

    fix: Edge scroll issues
    https://codepen.io/runjuu/pen/RmqZMB

--- src/Bar/Bar.styled.ts
+++ src/Bar/Bar.styled.ts
@@ -32,13 +32,10 @@ export const StyledBar = styled('div', {
 export const StyledInteractiveArea = styled.div<StyledInteractiveAreaProps>(
   ({ top = 0, right = 0, bottom = 0, left = 0, vertical }) => ({
     boxSizing: 'content-box',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
     width: '100%',
     height: '100%',
-    transform: 'translate(-50%, -50%)',
     padding: `${top}px ${right}px ${bottom}px ${left}px`,
+    margin: `-${top}px -${right}px -${bottom}px -${left}px`,
     cursor: vertical ? 'row-resize' : 'col-resize',
     WebkitTapHighlightColor: 'transparent',
     userSelect: 'none', // disable ios long press popup
